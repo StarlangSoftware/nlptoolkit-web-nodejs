b3b1d7be79c308d53e828ced9641a7c5f171fbe6
olcaytaner <olcay.yildiz@ozyegin.edu.tr>
2026-02-20 22:23:41 +0300

    Updated port number.

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const turkishDictionary = new TxtDictionary();
 const fsm = new FsmMorphologicalAnalyzer()
